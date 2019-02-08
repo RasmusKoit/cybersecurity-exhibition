40e58d55101cfe3ddbb9e3555ceb5727ad1b8f8f
Ebakoba <kaarelallemann@gmail.com>
2019-02-09 01:18:45 +0200

    moved to seperate victim and attacker

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import Koa from 'koa';
 import serve from 'koa-static';
-import publicWifi from './public-wifi';
+import publicWifi from './public-wifi/victim';
 const app = new Koa();
 
 const pageName = 'PUBLIC_WIFI';
 
 switch (pageName) {
   case 'PUBLIC_WIFI':
-    app.use(serve('./public-wifi/client'));
+    app.use(serve('./public-wifi/victim/client'));
     app.use(publicWifi.routes()).use(publicWifi.allowedMethods());
     break;
   default:
