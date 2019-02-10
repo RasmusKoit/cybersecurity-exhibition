04b581b0338f034d722b389a68972f9d520853f5
ebakoba <kaarelallemann@gmail.com>
2019-02-10 12:12:02 +0200

    commit front-end

--- command-control/index.js
+++ command-control/index.js
@@ -3,6 +3,7 @@ import bodyParser from 'koa-bodyparser';
 import Router from 'koa-router';
 import serve from 'koa-static';
 import { exec } from 'child_process';
+import shellescape from 'shell-escape';
 
 /**
  * Execute simple shell command (async wrapper).
@@ -24,18 +25,33 @@ async function sh(cmd) {
 const app = new Koa(); 
 const router = new Router();
 
-router.post('/api/container/:name', (ctx) => {
+router.get('/api/container', async (ctx) => {
+  const { stdout } = await sh('docker container list');
+
+  const containers = (stdout.split('\n').slice(1));
+  containers.pop();
+  console.log(containers)
+
+  ctx.body = containers;
+})
+
+router.post('/api/container/:name', async  (ctx) => {
   const { name } = ctx.params;
   const { options: { port } } = ctx.request.body;
 
-  sh(`docker run  -d -p ${port}:3000 ${name}`)
+  const result = await sh(shellescape(['docker', 'run' , '-d', '-p', `${port}:8080`, name]));
+  ctx.body = result.stdout;
 });
 
-router.delete('/api/container/:name', (ctx) => {
+router.delete('/api/container/:name', async (ctx) => {
+  const { name } = ctx.params;
 
+  const result = await sh(`docker rm $(docker stop $(docker ps -a -q --filter ancestor=${shellescape([name])} --format="{{.ID}}"))`)
+  ctx.status = 200;
 });
 
 app.use(bodyParser())
 app.use(serve('./client'));
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(3000);
