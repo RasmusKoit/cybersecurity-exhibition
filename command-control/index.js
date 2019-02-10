e4c9947da34a7a73f46d41a1b7c72da161779c59
ebakoba <kaarelallemann@gmail.com>
2019-02-10 09:50:48 +0200

    random commit

--- command-control/index.js
+++ command-control/index.js
@@ -1,9 +1,41 @@
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import Router from 'koa-router';
 import serve from 'koa-static';
+import { exec } from 'child_process';
 
+/**
+ * Execute simple shell command (async wrapper).
+ * @param {String} cmd
+ * @return {Object} { stdout: String, stderr: String }
+ */
+async function sh(cmd) {
+  return new Promise(function (resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
+}
 
 const app = new Koa(); 
+const router = new Router();
 
+router.post('/api/container/:name', (ctx) => {
+  const { name } = ctx.params;
+  const { options: { port } } = ctx.request.body;
+
+  sh(`docker run  -d -p ${port}:3000 ${name}`)
+});
+
+router.delete('/api/container/:name', (ctx) => {
+
+});
+
+app.use(bodyParser())
 app.use(serve('./client'));
 
 app.listen(3000);
--- deep-fake/main.py
+++ deep-fake/main.py
@@ -64,4 +64,4 @@ def make_app():
 
 if __name__ == '__main__':
   app = make_app()
-  app.run(port=8080)
+  app.run('0.0.0.0', port=8080)
